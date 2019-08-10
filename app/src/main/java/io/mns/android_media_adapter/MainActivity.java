86df3740757f37d1efba564e812fc683154cc04e
Mehdi Nosrati <nosratimehdi1999@gmail.com>
2019-08-10 16:44:09 +0430

    initial commit

---
+++ app/src/main/java/io/mns/android_media_adapter/MainActivity.java
@@ -0,0 +1,13 @@
+package io.mns.android_media_adapter;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
